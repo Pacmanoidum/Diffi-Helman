48516c74b6b794d8847b003f76b18d87d29869a0
Andrey_F <Andrey_F@HOME-PC>
2023-06-22 13:05:55 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Алгоритм_Диффи_Хелмана
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.a = new System.Windows.Forms.TextBox();
+            this.g = new System.Windows.Forms.TextBox();
+            this.p = new System.Windows.Forms.TextBox();
+            this.b = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.AL = new System.Windows.Forms.TextBox();
+            this.BL = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.k1 = new System.Windows.Forms.TextBox();
+            this.k2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(98, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "g";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(179, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "p";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(270, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "b";
+            // 
+            // a
+            // 
+            this.a.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a.Location = new System.Drawing.Point(4, 52);
+            this.a.Multiline = true;
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(54, 29);
+            this.a.TabIndex = 4;
+            // 
+            // g
+            // 
+            this.g.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.g.Location = new System.Drawing.Point(79, 52);
+            this.g.Multiline = true;
+            this.g.Name = "g";
+            this.g.Size = new System.Drawing.Size(61, 29);
+            this.g.TabIndex = 5;
+            // 
+            // p
+            // 
+            this.p.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.p.Location = new System.Drawing.Point(162, 52);
+            this.p.Multiline = true;
+            this.p.Name = "p";
+            this.p.Size = new System.Drawing.Size(59, 29);
+            this.p.TabIndex = 6;
+            // 
+            // b
+            // 
+            this.b.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b.Location = new System.Drawing.Point(246, 52);
+            this.b.Multiline = true;
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(60, 29);
+            this.b.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(4, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 46);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(164, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 45);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "clean";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(50, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "A";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(224, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "B";
+            // 
+            // AL
+            // 
+            this.AL.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AL.Location = new System.Drawing.Point(21, 198);
+            this.AL.Multiline = true;
+            this.AL.Name = "AL";
+            this.AL.ReadOnly = true;
+            this.AL.Size = new System.Drawing.Size(100, 35);
+            this.AL.TabIndex = 12;
+            // 
+            // BL
+            // 
+            this.BL.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BL.Location = new System.Drawing.Point(190, 198);
+            this.BL.Multiline = true;
+            this.BL.Name = "BL";
+            this.BL.ReadOnly = true;
+            this.BL.Size = new System.Drawing.Size(97, 35);
+            this.BL.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(50, 267);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 19);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "key 1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(205, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 19);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "key 2";
+            // 
+            // k1
+            // 
+            this.k1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.k1.Location = new System.Drawing.Point(21, 305);
+            this.k1.Multiline = true;
+            this.k1.Name = "k1";
+            this.k1.ReadOnly = true;
+            this.k1.Size = new System.Drawing.Size(100, 35);
+            this.k1.TabIndex = 16;
+            // 
+            // k2
+            // 
+            this.k2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.k2.Location = new System.Drawing.Point(193, 305);
+            this.k2.Multiline = true;
+            this.k2.Name = "k2";
+            this.k2.ReadOnly = true;
+            this.k2.Size = new System.Drawing.Size(94, 35);
+            this.k2.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 359);
+            this.Controls.Add(this.k2);
+            this.Controls.Add(this.k1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.BL);
+            this.Controls.Add(this.AL);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.p);
+            this.Controls.Add(this.g);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Диффи-хелман";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox a;
+        private System.Windows.Forms.TextBox g;
+        private System.Windows.Forms.TextBox p;
+        private System.Windows.Forms.TextBox b;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox AL;
+        private System.Windows.Forms.TextBox BL;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox k1;
+        private System.Windows.Forms.TextBox k2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Алгоритм_Диффи_Хелмана
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (((ulong.TryParse(a.Text, out ulong r1) & (ulong.TryParse(b.Text, out ulong r2)) & (ulong.TryParse(g.Text, out ulong r3)) & (ulong.TryParse(p.Text, out ulong r4))) == false))
+                MessageBox.Show("Заполните поля числами!");
+            else
+            {
+                ulong a1 = ulong.Parse(a.Text);
+                ulong g1 = ulong.Parse(g.Text);
+                ulong p1 = ulong.Parse(p.Text);
+                ulong b1 = ulong.Parse(b.Text);
+                AL.Text += Math.Pow(g1, a1) % p1;
+                BL.Text += Math.Pow(g1, b1) % p1;
+                k1.Text += Math.Pow(double.Parse(BL.Text), a1) % p1;
+                k2.Text += Math.Pow(double.Parse(AL.Text), b1) % p1;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a.Text = "";
+            g.Text = "";
+            p.Text = "";
+            b.Text = "";
+            AL.Text = "";
+            BL.Text = "";
+            k1.Text = "";
+            k2.Text = "";
+        }
+    }
+}
